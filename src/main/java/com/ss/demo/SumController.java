5ebc8e70e161adeb9dfb59e2e6d3e699dfc1f12b
TQFP64 <ann@gmail.com>
2011-06-20 17:05:52 +0300

    init

---
+++ src/main/java/com/ss/demo/SumController.java
@@ -0,0 +1,42 @@
+package com.ss.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * This example shows usage of request.getParameter("param_name")
+ */
+@Controller
+public class SumController extends AbstractController {
+    private int count = 0;
+
+    @RequestMapping(value="/sum", method= RequestMethod.GET)
+    protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        ModelAndView mav = new ModelAndView("calc");
+        try {
+            count++;
+            String x = request.getParameter("x");
+            String y  = request.getParameter("y");
+            if (x == null || x.length() == 0) {
+                 x = "0";
+            }
+            if (y == null || y.length() == 0) {
+                y = "0";
+            }
+            int sum = Integer.parseInt(x) + Integer.parseInt(y);
+            mav.addObject("sum", sum);
+            mav.addObject("x", x);
+            mav.addObject("y", y);
+            mav.addObject("count", count);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return mav;
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/calc.jsp
@@ -0,0 +1,19 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Calculator</title>
+  </head>
+  <body>
+    <form>
+        <input type="text" name="x" value="${x}"/>
+        +
+        <input type="text" name="y" value="${y}"/>
+        = ${sum}
+        <br/>
+        <input type="submit" value="Calc"/>
+    </form>
+    <hr/>
+    <a href="../index">Home</a>
+    <b>&nbsp;&nbsp;SumController.handleRequestInternal()</b> called ${count} times
+  </body>
+</html>
